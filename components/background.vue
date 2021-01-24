146358340b1b491c1bc616d75e67b823e8dfce63
dimitrifranov <dfranov01@gmail.com>
2021-01-24 11:40:16 +0100

    added tiling classes

--- components/background.vue
+++ components/background.vue
@@ -5,10 +5,34 @@
 <script>
 export default {
   mounted() {
+    const heigth = 10
     const canvas = document.getElementById('background')
     const ctx = canvas.getContext('2d')
     ctx.fillStyle = 'blue'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    // ctx.fillRect(0, 0, canvas.width, canvas.height)
+    class Tile {
+      constructor() {
+        this.height = heigth
+      }
+    }
+    class Kite extends Tile {
+      constructor(height, x, y) {
+        super(height)
+        this.x = x
+        this.y = y
+      }
+
+      draw() {
+        ctx.strokeStyle = 'red'
+        ctx.beginPath()
+        ctx.lineWidth = '1'
+        ctx.moveTo(this.x, this.y)
+        ctx.lineTo(250, 75)
+        ctx.stroke()
+      }
+    }
+    const firstKite = new Kite(10, 0, 0)
+    firstKite.draw()
   },
 }
 </script>
